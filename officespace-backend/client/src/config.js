dbb41993ddf1d06f731804a60a6f320dc0abafd5
jdloude <jdloude@gmail.com>
2018-04-27 18:44:30 -0600

    testing

--- officespace-backend/client/src/config.js
+++ officespace-backend/client/src/config.js
@@ -1,6 +1,9 @@
 import { accessKeyId, secretAccessKey } from './s3-config.json'
+import {AWS} from 'aws-sdk';
  const production = false;
 
+ console.log('this is the data: ', process.env.AWS_SDK_LOAD_CONFIG);
+ 
  export const apiUrl = production ? 'http://domain.com/api' : 'http://localhost:3000/api';
 // export const smtp = {
 //     host: 'smtp.sendgrid.net',
